8a84aed625f2ee43da2b667d7477c57bb227ccec
Roman2018122 <jonathantorres1118@Gmail.com>
2026-05-21 20:01:32 -0500

    CLASE 21/05/2026/

--- modulo3/dart/05_switch.dart
+++ modulo3/dart/05_switch.dart
@@ -1,3 +1,4 @@
+
 void main() {
   String codigoHttp = '404';
 
--- modulo3/dart/10_funciiones.dart
+++ modulo3/dart/10_funciiones.dart
@@ -24,6 +24,23 @@ int multiplicar (int a, int b) =>a*b;
 void saludarOpcional(String nombre, [String apellido = 'Sin apeliido']){
   print('Hola $nombre $apellido');
 }
+
+//Funcion con parametros nombrados
+void registroUsuario({
+  required String nombre,
+  required int edad,
+}){
+  print("Hola $nombre, $edad");
+}
+
+
+// Con tipo explícito — recomendado
+String formatearPrecio(double precio) => '\$${precio.toStringAsFixed(2)}';
+
+// Sin tipo — Dart infiere que retorna String
+formatearPrecioSinTipo(double precio) => '\$${precio.toStringAsFixed(2)}';
+
+
 void main() {
   saludar();
   saludoConParametro('JOnathan ROman');
@@ -33,4 +50,11 @@ void main() {
   print('La multiplicacion es: ${multiplicar(5, 6)}');  
   saludarOpcional('Jonathan', 'Torres');
   saludarOpcional('Jonathan');
+  registroCliente(
+    nombre: 'ana',
+    edad: 22
+  );
+  print(formatearPrecio(1299.9)); //$1299.90
+  print(formatearPrecioSinTipo(1299.9)); //1299.90
 } 
+
